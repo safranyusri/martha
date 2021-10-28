6e67308e6ae6bf2e65fe7a84986f59a75fcb6865
Safran Yusri <safranyusri@users.noreply.github.com>
2021-10-28 19:45:56 +0700

    Create 001.js

---
+++ training/001.js
@@ -0,0 +1,23 @@
+// Simple Javascript and Object Type
+// String
+var greeting = 'hello world';
+print(greeting);
+//Number
+var number = 42;
+print(number);
+//List
+var myList = ['a',greeting, number];
+print(myList);
+
+//Dictonary
+var myDict = {
+  'a': number,
+  'b': myList,
+  'c':1
+};
+print(myDict);
+//Functions
+var myFunction = function(input){
+  return input
+};
+print(myFunction('hello!'));
