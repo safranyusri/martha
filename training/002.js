5c06657f09de071a2d906c1aa1c6394ace0a53f7
Safran Yusri <safranyusri@users.noreply.github.com>
2021-10-28 19:46:26 +0700

    Create 002.js

---
+++ training/002.js
@@ -0,0 +1,14 @@
+// Get SRTM
+var srtm = ee.Image('USGS/SRTMGL1_003');
+
+// Zoom to Puncak Jaya
+Map.setCenter(137.930, -4.444, 9);
+
+//Display on map
+Map.addLayer(srtm);
+
+// Select elevation band
+var elevation = srtm.select('elevation');
+
+// Display on map with ranges and layer name
+Map.addLayer(elevation,{min: 0, max: 4000},'elevation');
